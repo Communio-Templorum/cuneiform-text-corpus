85dd427fcac30eea9b53506632f9dad96d293602
Sam Grundman <samuel@yodas.ws>
2019-11-03 00:13:14 -0400

    Get site back into working order
    required adding new page-load events to track

--- src/app.js
+++ src/app.js
@@ -1,17 +1,16 @@
 /* app.json */
 // import Litedom from 'res/litedom.es.js';
-yodasws.page('home').setRoute({
-	template: 'pages/home.html',
-	route: '/',
-}).on('load', () => {
+yodasws.on('site-loaded', () => {
+	document.querySelectorAll('body > nav a:not([href])').forEach((link) => {
+		link.addEventListener('click', toggleMenu);
+	});
 
-	function toggleMenu(e) {
-		if (document.body.getAttribute('y-page') === 'home') return;
-		let next = e.currentTarget;
+	function toggleMenu(evt) {
+		let next = evt.currentTarget;
 		do next = next.nextElementSibling;
 		while (next && next.nodeName.toLowerCase() !== 'ul');
 		const isHidden = next && next.hasAttribute('hidden');
-		const parent = e.currentTarget.closest('ul, nav');
+		const parent = evt.currentTarget.closest('ul, nav');
 		if (parent instanceof Element) {
 			parent.querySelectorAll('ul:not([hidden])').forEach((list) => {
 				list.setAttribute('hidden', '');
@@ -24,46 +23,50 @@ yodasws.page('home').setRoute({
 
 	// Adjustments to Display of Nav for UX
 	new MutationObserver((mutationsList) => {
+		console.log('mutationsList:', mutationsList);
 		switch (mutationsList[0].oldValue) {
-		case 'home':
-			const navLists = document.querySelectorAll('body > nav ul');
-			// No Transition
-			navLists.forEach((list) => {
-				list.style.transition = 'none';
-				list.setAttribute('hidden', '');
-			});
-			// Reallow Transition after page change
-			setTimeout(() => {
+			case 'home': {
+				const navLists = document.querySelectorAll('body > nav ul');
+				// No Transition
 				navLists.forEach((list) => {
-					list.style.transition = '';
+					list.style.transition = 'none';
+					list.setAttribute('hidden', '');
 				});
-			}, 0);
-			break;
-		case null:
-			// Be sure all menus are collapsed
-			document.querySelectorAll('body > nav ul').forEach((list) => {
-				list.setAttribute('hidden', '');
-			});
-			// Page loaded, show nav menu
-			const nav = document.querySelector('body > nav[hidden]');
-			if (nav instanceof Element) nav.removeAttribute('hidden');
+				// Reallow Transition after page change
+				setTimeout(() => {
+					navLists.forEach((list) => {
+						list.style.transition = '';
+					});
+				}, 0);
+				break;
+			}
+			case null:
+				// Be sure all menus are collapsed
+				document.querySelectorAll('body > nav ul').forEach((list) => {
+					list.setAttribute('hidden', '');
+				});
 		}
 	}).observe(document.body, {
-		attributeFilter: ['ng-section'],
+		attributeFilter: ['y-section'],
 		attributeOldValue: true,
 		attributes: true,
 	});
 
-	document.querySelectorAll('body > nav a:not([href])').forEach((link) => {
-		link.addEventListener('click', toggleMenu);
-	});
+	// Site loaded, show nav menu
+	const nav = document.querySelector('body > nav[hidden]');
+	if (nav instanceof Element) nav.removeAttribute('hidden');
+});
 
+yodasws.on('page-loaded', (evt) => {
 	// Close Menu on Navigation
-	window.addEventListener('hashchange', () => {
-		document.querySelectorAll('body > nav ul:not([hidden])').forEach((list) => {
-			list.setAttribute('hidden', '');
-		});
+	document.querySelectorAll('body > nav ul:not([hidden])').forEach((list) => {
+		list.setAttribute('hidden', '');
 	});
+	document.body.setAttribute('y-section', evt.detail.page);
+});
+yodasws.page('home').setRoute({
+	template: 'pages/home.html',
+	route: '/',
 });
 
 yodasws.page('etcsl').setRoute({
